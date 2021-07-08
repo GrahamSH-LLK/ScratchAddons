bfd85e058a60330f219a555083a87b9dbe3f1b34
GrahamSH <grahamshllk@gmail.com>
2021-07-08 14:47:59 -0400

    Hijacking innocent coders :tm:

--- addons/project-download-button/userscript.js
+++ addons/project-download-button/userscript.js
@@ -1,10 +1,19 @@
 // borrows from project-info/projectstats.js
-
+import downloadBlob from '/libraries/common/cs/download-blob.js'
 export default async function ({ addon, console, msg }) {
 
   const vm = addon.tab.traps.vm;
+  
 
   console.log(vm);
+  const getProjectFilename = (curTitle, defaultTitle) => {
+      let filenameTitle = curTitle;
+      if (!filenameTitle || filenameTitle.length === 0) {
+          filenameTitle = defaultTitle;
+      }
+      return `${filenameTitle.substring(0, 100)}.sb3`;
+  };
+
 
   async function insertDownloadButton() {
 
@@ -23,7 +32,11 @@ export default async function ({ addon, console, msg }) {
     let downloadButton = document.createElement('button');
     downloadButton.className = "button remix-button download-button"; // include remix-button class to inherit icon styling
     downloadButton.innerText = msg('download');
-
+    downloadButton.addEventListener('click', (e) => {
+      let filename = getProjectFilename(addon.tab.redux.state.scratchGui.projectTitle, '');
+      let blob = await vm.saveProjectSb3();
+      downloadBlob(filename, blob);
+    });
     buttons.insertBefore(downloadButton, buttons.firstChild);
   }
 
